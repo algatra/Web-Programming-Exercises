6a45726c4105b2bbeea3832734f12c1a9d61acc4
AlFarkhan <55190771+algatra@users.noreply.github.com>
2021-04-05 07:16:37 +0700

    update dikit

--- Praktikum 03/Kegiatan 2/p030202.php
+++ Praktikum 03/Kegiatan 2/p030202.php
@@ -2,18 +2,19 @@
 
 	function hitungDenda($jadwalKembali, $tglKembali){
 
-		if (strtotime($tglKembali) <= strtotime($jadwalKembali)) {
-			return 0;
-		}
+		if (strtotime($tglKembali) > strtotime($jadwalKembali)) {
 		
-		$jadwal = new DateTime($jadwalKembali);
-		$kembali = new DateTime($tglKembali);
-		$telat = $kembali -> diff($jadwal) -> days;
+			$jadwal = new DateTime($jadwalKembali);
+			$kembali = new DateTime($tglKembali);
+			$telat = $kembali -> diff($jadwal) -> days;
 
-		return $telat * 3000;
+			return $telat * 3000;
+		}
+
+		return 0;
 	}
 
-	$denda = hitungDenda("2021-03-20", "2021-03-21");
+	$denda = hitungDenda("2021-03-20", "2021-03-15");
 	echo "Besarnya denda adalah: Rp $denda";
 
 ?>
