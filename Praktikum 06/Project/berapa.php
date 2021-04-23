7a27bace5c01466e730a7ce5512e4fd230a84930
AlFarkhan <55190771+algatra@users.noreply.github.com>
2021-04-23 16:53:16 -0700

    lupa ngasih login

--- Praktikum 06/Project/berapa.php
+++ Praktikum 06/Project/berapa.php
@@ -1,14 +1,20 @@
 <?php  
 
-	if ((!isset($_COOKIE['bilrand'])) or (isset($_COOKIE['bilrand']) && ($_COOKIE['bilrand'] == null)) ) {
-		setcookie("bilrand", rand(0,100), time()+7*24*3600,"/");
+	if (!isset($_COOKIE['nama'])) {
+		echo "<script>alert('Hayooo, login dulu dong');";
+		header("Location: login.php");
 	}
 
-	echo "<p>Hallo, Nama saya Mr Math. Saya telah memilih sebuah bilangan integer 0-100. Silahkan ditebak yaa ^_^</p>";
-	// var_dump($_COOKIE['bilrand']);
+	if ($_COOKIE['bilrand'] == null) {
+		setcookie("bilrand", rand(0,100), time()+7*24*3600, '/');
+		header("Location: berapa.php");
+	} 
+
+	echo "<p>Hallo ".$_COOKIE['nama'].", Nama saya Mr Math. Saya telah memilih sebuah bilangan integer 0-100. Silahkan ditebak yaa ^_^</p>";
+	var_dump($_COOKIE['bilrand']);
 
 ?>
-	<form method="POST" action="p2.php">
+	<form method="POST" action="berapa.php">
 		Masukkan Angka : <input type="text" name="bilask">
 		<input type="submit" name="submit">
 	</form>
@@ -22,9 +28,10 @@
 			echo "<p style='color:red;'>Waduuhh, terlalu besarr. Coba tebak lagi !!</p>";
 		} else if ($_POST['bilask'] == ((int)$_COOKIE['bilrand'])) {
 			echo "<h2 style='color:green';>Selamat! Tebakan Kamu Benarr, Angka Itu Adalah ".$_COOKIE['bilrand']."</h2>";
-			setcookie("bilrand", null, -1, '/');
+			setcookie("bilrand", null, time()+2, '/');
 			unset($_POST['bilask']);
-			echo '<a style="text-decoration: none;" href="p2.php">Mulai Lagi</a>';
+			echo '<p><a style="text-decoration: none;" href="berapa.php">Mulai Lagi</a></p>';
+			echo '<p><a style="text-decoration: none;" href="exit.php">Exit</a></p>';
 		}
 		unset($_POST['bilask']);
 		
